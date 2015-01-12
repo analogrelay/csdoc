430d610b43e33a2ae6e1786dce1b59f8627f4f72
anurse <andrew@andrewnurse.net>
2015-01-12 15:59:15 -0800

    Initial Commit

---
+++ src/NetDoc/DocumentationVisitor.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace NetDoc
+{
+    public class DocumentationVisitor : SymbolVisitor
+    {
+        public override void VisitAssembly(IAssemblySymbol symbol)
+        {
+            symbol.GlobalNamespace.Accept(this);
+        }
+
+        public override void VisitNamespace(INamespaceSymbol symbol)
+        {
+            foreach (var child in symbol.GetMembers())
+            {
+                child.Accept(this);
+            }
+        }
+
+        public override void VisitNamedType(INamedTypeSymbol symbol)
+        {
+            foreach (var member in symbol.GetMembers())
+            {
+                member.Accept(this);
+            }
+        }
+
+        //- This is a test
+        //- It has two lines
+        public override void VisitMethod(IMethodSymbol symbol)
+        {
+            if (symbol.MethodKind == MethodKind.Ordinary)
+            {
+                string asm = symbol.ContainingAssembly.Name;
+                string type = symbol.ContainingType.Name;
+                string ns = symbol.ContainingNamespace.Name;
+
+                System.Console.WriteLine(asm + ":" + ns + "." + type + "#" + symbol.Name);
+
+                var declaration = symbol.DeclaringSyntaxReferences.FirstOrDefault();
+                if (declaration != null)
+                {
+                    var comments = declaration
+                        .GetSyntax()
+                        .GetLeadingTrivia()
+                        .Where(t => t.CSharpKind() == SyntaxKind.SingleLineCommentTrivia)
+                        .Select(t => new { Trivia = t, Text = t.ToFullString() })
+                        .Where(t => t.Text.StartsWith("//-"))
+                        .Select(t => new { t.Trivia, Text = t.Text.Substring(3) });
+                    foreach (var comment in comments)
+                    {
+
+                        System.Console.WriteLine(" " + comment.Text);
+                    }
+                }
+
+            }
+            base.VisitMethod(symbol);
+        }
+    }
+}
---
+++ src/NetDoc/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Runtime.Roslyn;
+
+namespace NetDoc
+{
+    public class Program
+    {
+        private readonly ICache _cache;
+        private readonly IFileWatcher _watcher;
+        private readonly IServiceProvider _services;
+        private readonly ICacheContextAccessor _cacheContextAccessor;
+        private readonly IAssemblyLoadContextFactory _assemblyLoadContextFactory;
+        private readonly INamedCacheDependencyProvider _namedCacheDependencyProvider;
+
+        private readonly IApplicationEnvironment _env;
+        private readonly ILibraryManager _libraryManager;
+        private readonly IProjectResolver _projectResolver;
+
+        public Program(
+            ICache cache,
+            IFileWatcher watcher,
+            IServiceProvider services,
+            ICacheContextAccessor cacheContextAccessor,
+            IAssemblyLoadContextFactory assemblyLoadContextFactory,
+            INamedCacheDependencyProvider namedCacheDependencyProvider,
+            IProjectResolver projectResolver,
+            ILibraryManager libraryManager,
+            IApplicationEnvironment env)
+        {
+            _cache = cache;
+            _watcher = watcher;
+            _services = services;
+            _cacheContextAccessor = cacheContextAccessor;
+            _assemblyLoadContextFactory = assemblyLoadContextFactory;
+            _namedCacheDependencyProvider = namedCacheDependencyProvider;
+
+            _projectResolver = projectResolver;
+            _libraryManager = libraryManager;
+            _env = env;
+        }
+
+        public int Main(string[] args)
+        {
+            Project prj;
+            if (!_projectResolver.TryResolveProject(_env.ApplicationName, out prj)) {
+                Console.Error.WriteLine("Failed to resolve project");
+                return 1;
+            }
+
+            var rootCacheContext = new CacheContext(new object(), _ => { });
+            _cacheContextAccessor.Current = rootCacheContext;
+
+            var compiler = new RoslynCompiler(
+                _cache,
+                _cacheContextAccessor,
+                _namedCacheDependencyProvider,
+                _assemblyLoadContextFactory,
+                _watcher,
+                _services);
+            var key = new LibraryKey()
+            {
+                Aspect = null,
+                Name = prj.Name,
+                Configuration = _env.Configuration,
+                TargetFramework = _env.RuntimeFramework
+            };
+            var export = _libraryManager.GetLibraryExport(prj.Name);
+            var refr = export.MetadataReferences
+                .OfType<IMetadataProjectReference>()
+                .FirstOrDefault(m => string.Equals(m.Name, prj.Name, StringComparison.OrdinalIgnoreCase));
+            if (refr == null)
+            {
+                Console.Error.WriteLine("Unable to get metadata reference");
+                return 2;
+            }
+
+            List<IMetadataReference> references = new List<IMetadataReference>();
+            var result = compiler.CompileProject(
+                prj,
+                key,
+                export.MetadataReferences,
+                export.SourceReferences,
+                references);
+
+            // Visit syntax trees
+            var docVisitor = new DocumentationVisitor();
+            result.Compilation.Assembly.Accept(docVisitor);
+
+            return 0;
+        }
+
+        private class LibraryKey : ILibraryKey
+        {
+            public string Aspect { get; set; }
+            public string Configuration { get; set; }
+            public string Name { get; set; }
+            public FrameworkName TargetFramework { get; set; }
+        }
+    }
+}
